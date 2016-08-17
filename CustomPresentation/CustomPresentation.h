8cd7dab04a7f30e809c09cdb4149b101241d775a
Dylan <dylan@dylanedwards.net>
2016-08-17 15:50:26 -0700

    Initial Commit

---
+++ CustomPresentation/CustomPresentation.h
@@ -0,0 +1,19 @@
+//
+//  CustomPresentation.h
+//  CustomPresentation
+//
+//  Created by Dylan Edwards on 8/17/16.
+//  Copyright © 2016 Slinging Pixels Media. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CustomPresentation.
+FOUNDATION_EXPORT double CustomPresentationVersionNumber;
+
+//! Project version string for CustomPresentation.
+FOUNDATION_EXPORT const unsigned char CustomPresentationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CustomPresentation/PublicHeader.h>
+
+
